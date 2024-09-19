fa2e28028e08d839f9ed2fd8377c5dab230e3342
Ignacio <nachohcorrea@gmail.com>
2024-09-19 13:10:34 -0300

    SCRUM-26: context y routes configuradas

--- switcher/src/main.jsx
+++ switcher/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense, lazy } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import { GameProvider } from './contexts/GameContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Home = lazy(() => import('./pages/Home'));
+// const Games = lazy(() => import('./Games'));
+
+const Loader = () => <div>Loading...</div>;
+
+const Main = () => (
+  <GameProvider>
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/games" element={<Games />} /> */}
+          {/* <Route path="/games/create" element={<CreateGame />} /> */}
+          {/* <Route path="/games/lobby/:gameId" element={<Lobby />} /> */}
+          {/* <Route path="/games/ongoing/:gameId" element={<ActiveGame />} /> */}
+          {/* <Route path="/winner" element={<Winner />} /> */}
+        </Routes>
+      </Suspense>
+    </Router>
+  </GameProvider>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);

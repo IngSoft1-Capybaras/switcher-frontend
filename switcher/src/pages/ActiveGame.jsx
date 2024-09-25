709bcd1399839be8cfb7a7e5604473a84bc19cbe
ArmandMC <armando.carral@mi.unc.edu.ar>
2024-09-25 17:11:37 -0300

    SCRUM 14: Implementado el componente con su test. Lo estuve trabanjando a la par con el componente de cartas de movimiento, ahora estan modularizados.

--- switcher/src/pages/ActiveGame.jsx
+++ switcher/src/pages/ActiveGame.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { useGameContext } from '../context/GameContext'; // Asegúrate de que esta ruta sea correcta
-import CardsMoviment from '../components/ui/cardsMoviment';
 import CardsFigure from '../components/ui/cardsFigure';
 
 const ActiveGame = () => {
@@ -9,12 +8,6 @@ const ActiveGame = () => {
   return (
     <div>
       <h1>Juego Activo</h1>
-
-      {/* Renderiza las cartas de movimiento del jugador actual */}
-      <div className="flex space-x-4">
-        <h2>Cartas de Movimiento</h2>
-        <CardsMoviment gameId={gameId} playerId={playerId} />
-      </div>
 
       {/* Renderiza las cartas de figura del jugador actual */}
       <div className="flex space-x-8">

7bef15972c03b26981bd3f208776130cd0393314
GasparSaenzValiente <gaspar.saenz.valiente@mi.unc.edu.ar>
2024-09-29 13:17:09 -0300

    SCRUM-16 trato de añadir sockets

--- switcher/src/components/ui/TurnInfo.jsx
+++ switcher/src/components/ui/TurnInfo.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useGameContext } from "@/context/GameContext"; 
 import { useSocketContext } from "@/context/SocketContext"; 
 
-export default function TurnInformation() {
+export default async function TurnInformation() {
     const { playerId, activeGameId, currentTurn, setCurrentTurn } = useGameContext();
-    
+    const [turnData, setTurnData] = useState(null);
+    const {socket} = useSocketContext();
+
     const fetchTurnInfo = async (activeGameId) => {
         try {
             const response = await fetch(`http://localhost:8000/game_state/${activeGameId}/current_turn`, {
@@ -25,4 +27,54 @@ export default function TurnInformation() {
         }
     }
 
-}
+
+    useEffect(() => {
+        if (!socket) return;
+
+        const handleNextTurnEvent = async (event) => {
+            const data = JSON.parse(event.data);
+
+            if (data.type === "NEXT_TURN") {
+                
+                // seteamos nuevo turno en GameContext
+                setCurrentTurn(data.nextPlayerId);
+                
+                // Get de la informacion del nuevo turno
+                const newTurnData = await fetchTurnInfo(activeGameId);
+                
+                // Actualizamos el turno actual
+                setTurnData(newTurnData); 
+            }
+        };
+
+        socket.addEventListener("message", handleNextTurnEvent);
+
+        return () => {
+          socket.removeEventListener("message", handleNextTurnEvent);
+        };
+      }, [socket, playerId]);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 
+};

341f41b42b5085855b309ff4dc5c10b319d245b2
GasparSaenzValiente <gaspar.saenz.valiente@mi.unc.edu.ar>
2024-10-11 16:07:02 -0300

    SCRUM-72 falta tooltip test

--- switcher/src/tests/ClaimFigureButton.test.jsx
+++ switcher/src/tests/ClaimFigureButton.test.jsx
@@ -5,14 +5,14 @@ import{ render, screen, waitFor } from "@testing-library/react";
 import { describe, it, expect, beforeEach, vi } from "vitest";
 
 
-//disabled={!((currentTurn==playerId) && selectedCardFigure && box.highlighted)}
+// disabled={!(figure.length!==0 && (playerId == currentTurn) && cardId)}
 
 describe('Claim Figure Button', () => {
 
-  mockPlayerId = 1;
-  mockGameId = 5;
-  mockCardId = 10;
-  mockFigure = [
+  const mockPlayerId = '1';
+  const mockGameId = '5';
+  const mockCardId = '10';
+  const mockFigure = [
     { pos_x: 0, pos_y: 2, color: "BLUE", highlighted: true },
     { pos_x: 0, pos_y: 3, color: "BLUE", highlighted: true },
     { pos_x: 0, pos_y: 4, color: "BLUE", highlighted: true },
@@ -23,23 +23,39 @@ describe('Claim Figure Button', () => {
     useGameContext: vi.fn(),
   }));
 
-
-  beforeAll(() => {
-    useGameContext.mockReturnValue({ playerId: mockPlayerId });
-  })
   afterEach(() => {
     vi.clearAllMocks();
   });
 
   it(`Should render de ClaimButton`, () => {
+    useGameContext.mockReturnValue({ playerId: mockPlayerId, currentTurn: '1' });
     render(<ClaimFigureButton gameId={mockGameId} cardId={mockCardId} figure={mockFigure}/>);
     const claimButton = screen.getByTestId('claimButtonTestId');
     expect(claimButton).toBeInTheDocument();
   });
 
+  it('Should be enabled when (figure.length!==0 && (playerId == currentTurn) && cardId)', () => {
+    useGameContext.mockReturnValue({ playerId: mockPlayerId, currentTurn: '1' });
+    render(<ClaimFigureButton gameId={mockGameId} cardId={mockCardId} figure={mockFigure}/>);
+    const claimButton = screen.getByTestId('claimButtonTestId');
+    expect(claimButton).toBeEnabled();
+  })
+
+  it('Should not be enabled when (figure.length!==0 && (playerId == currentTurn) && !cardId)', () => {
+    useGameContext.mockReturnValue({ playerId: mockPlayerId, currentTurn: '1' });
+    render(<ClaimFigureButton gameId={mockGameId} figure={mockFigure}/>);
+    const claimButton = screen.getByTestId('claimButtonTestId');
+    expect(claimButton).toBeDisabled();
+  })
+
+  it('Should be not enabled when (figure.length!==0 && !(playerId == currentTurn) && cardId)', () => {
+    useGameContext.mockReturnValue({ playerId: mockPlayerId, currentTurn: '2' });
+    render(<ClaimFigureButton gameId={mockGameId} cardId={mockCardId} figure={mockFigure}/>);
+    const claimButton = screen.getByTestId('claimButtonTestId');
+    expect(claimButton).toBeDisabled();
+  })
 
 
-
-
+  // checking tooltip
 
 })

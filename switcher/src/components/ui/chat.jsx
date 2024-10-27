6f63ab99ccc067dbb55549fa841da62bca2eba27
GasparSaenzValiente <gaspar.saenz.valiente@mi.unc.edu.ar>
2024-10-26 22:54:48 -0300

    SCRUM-157 chat funcional para una persona sola

--- switcher/src/components/ui/chat.jsx
+++ switcher/src/components/ui/chat.jsx
@@ -13,11 +13,15 @@ export default function Chat () {
 
   return (
     <div className="w-full md:w-2/3 md:ml-4 bg-zinc-900 p-4 rounded-lg shadow-md border border-zinc-800">
-        <h3 className="text-xl font-bold text-white mb-2">Chat</h3>
+      <h3 className="text-xl font-bold text-white mb-2">Chat</h3>
 
+      <div>
+        {chat.map((msg, index) => (
+          <div className="text-zinc-300 mb-1" key={index}>{msg}</div>
+        ))}
+      </div>
 
-
-
+      <div>
         <input
           type="text"
           className="w-full p-2 bg-zinc-900 text-zinc-300 rounded"
@@ -25,7 +29,11 @@ export default function Chat () {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button onClick={handleSendMessage}/>
+
+        <button onClick={handleSendMessage}>
+          Enviar
+        </button>
       </div>
+    </div>
   );
 }

dac997640331c469777917e46ea31ac0f5be98c2
GasparSaenzValiente <gaspar.saenz.valiente@mi.unc.edu.ar>
2024-11-04 18:31:06 -0300

    SCRUM-158 cambios en tama;o

--- switcher/src/components/ui/chat.jsx
+++ switcher/src/components/ui/chat.jsx
@@ -99,24 +99,25 @@ export default function Chat ({gameId}) {
 
   return (
     <AnimatePresence>
-        {!isMinimized ?
-        (<motion.div
-          className="w-full md:w-96 bg-zinc-900 p-4 rounded-lg shadow-md border border-zinc-800"
+      {!isMinimized ? (
+        <motion.div
+          className="w-full md:w-[32rem] max-w-[32rem] bg-zinc-900 p-4 rounded-lg shadow-md border border-zinc-800"
           key="expanded"
           initial={{ opacity: 0, scale: 0.95, y: 20 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ duration: 0.25 }}
         >
           <div className="flex items-center justify-between p-2 border-b border-zinc-800">
-              <h3 className="text-lg font-semibold text-white">Chat</h3>
-              <Button
-                onClick={() => setIsMinimized(true)}
-                className="text-zinc-400 hover:text-white transition-colors p-1 rounded-lg hover:bg-zinc-800"
-              >
-                <FaMinus />
-              </Button>
-            </div>
-          <ScrollArea id='chatScrollArea' className="h-60 mb-2 pr-3">
+            <h3 className="text-lg font-semibold text-white">Chat</h3>
+            <Button
+              onClick={() => setIsMinimized(true)}
+              className="text-zinc-400 hover:text-white transition-colors p-1 rounded-lg hover:bg-zinc-800"
+            >
+              <FaMinus />
+            </Button>
+          </div>
+
+          <ScrollArea id='chatScrollArea' className="h-80 mb-2 pr-3">
             {chat.map((msg, index) => {
               const isChatMessage = msg.includes(':');
               const sender = msg.split(':')[0];
@@ -125,22 +126,25 @@ export default function Chat ({gameId}) {
               const isCurrentUser = sender === username;
 
               return (
-                <motion.div key={index}
+                <motion.div
+                  key={index}
                   initial={{ opacity: 0, y: 10 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.2 }}
                   className={`flex ${isCurrentUser ? 'justify-end' : 'justify-start'} mb-1`}
                 >
                   <div
-                    className={`text-zinc-300 p-2 rounded-lg max-w-[75%] overflow-wrap break-words
-                                ${getPlayerColor(playerIndex)}`}
+                    className={`text-zinc-300 p-2 rounded-lg max-w-[85%] break-words
+                      ${getPlayerColor(playerIndex)}`}
                   >
                     {!isCurrentUser && isChatMessage && (
-                              <span className="text-sm text-zinc-400 block mb-1">
-                                {sender}
-                              </span>
-                            )}
-                    <p className="text-white">{msgContent || msg}</p>
+                      <span className="text-sm text-zinc-400 block mb-1">
+                        {sender}
+                      </span>
+                    )}
+                    <p className="sm:text-sm md:text-lg text-white whitespace-pre-wrap">
+                      {msgContent || msg}
+                    </p>
                   </div>
                 </motion.div>
               );
@@ -150,43 +154,39 @@ export default function Chat ({gameId}) {
           <form onSubmit={handleSendMessage} className='flex items-stretch'>
             <input
               type="text"
-              className="flex-1 bg-zinc-800 text-white rounded-l-full px-4 py-2 focus:outline-none "
+              className="flex-1 bg-zinc-800 text-white rounded-l-full px-4 py-2 focus:outline-none"
               placeholder="Escribe tu mensaje..."
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
-
             <Button
               type="submit"
-              className={`rounded-r-full p-3 bg-gray-500 hover:bg-gray-700 transition-colors h-auto`}
+              className="rounded-r-full p-3 bg-gray-500 hover:bg-gray-700 transition-colors h-auto"
             >
               <IoIosSend className='w-5 h-5'/>
             </Button>
           </form>
         </motion.div>
-        )
-        :
-        (
-          <motion.div
-            key="minimized"
-            initial={{ opacity: 0, y: 20}}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.25 }}
+      ) : (
+        <motion.div
+          key="minimized"
+          initial={{ opacity: 0, y: 20}}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.25 }}
+        >
+          <Button
+            onClick={handleChatClick}
+            className="w-full md:w-[32rem] p-4 justify-between hover:bg-zinc-800 rounded-lg bg-zinc-900 border border-zinc-800"
           >
-            <Button
-              onClick={handleChatClick}
-              className="w-full md:w-96 p-4 justify-between hover:bg-zinc-800 rounded-lg bg-zinc-900 border border-zinc-800"
-            >
-              <div className="flex items-center gap-2 overflow-hidden">
-                <IoIosChatboxes className="w-5 h-5 shrink-0" />
-                <span className="text-sm truncate">
-                  {chat.length > 0 ? (chat[chat.length - 1]) : "Abrir chat"}
-                </span>
-              </div>
-            </Button>
-          </motion.div>
-        )
-        }
+            <div className="flex items-center gap-2 overflow-hidden">
+              <IoIosChatboxes className="w-5 h-5 shrink-0" />
+              <span className="sm:text-sm md:text-lg truncate">
+                {chat.length > 0 ? (chat[chat.length - 1]) : "Abrir chat"}
+              </span>
+            </div>
+          </Button>
+        </motion.div>
+      )}
     </AnimatePresence>
   );
-}
+};
